deb726f141e122a54e034c88bb5ab32601cf36d1
volcan <volcan@drek>
2016-12-03 22:52:24 +0000

    This is broken, moving HarvestDirectoryOfSuppliers to it's own file

--- src/Harvester.py
+++ src/Harvester.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as bs
+from . import HarvestSupplierProfile
 class HarvestDirectoryOfSuppliers:
   BASE_URL="http://www.globalspec.com/SpecSearch/SuppliersByName/AllSuppliers/"
   SoupLiars=[]
@@ -13,36 +14,36 @@ class HarvestDirectoryOfSuppliers:
     }
     return ret
 
-  def harvestSuppliersFromPage(self,html):
-    soup = bs(html,'html.parser')
-    suppliers=soup.findAll("tr",attrs={"class":"result-item"})
-    if suppliers is None: return None #This is past the last page
-    for s in suppliers:
-      a=s.find("a")
-      link=a['href']
-      name=a.getText()
-      entry=self.genEntry(name.strip(),link.strip())
-      self.SoupLiars.append(entry)
+    def harvestSuppliersFromPage(self,html):
+       soup = bs(html,'html.parser')
+       suppliers=soup.findAll("tr",attrs={"class":"result-item"})
+       if suppliers is None: return None #This is past the last page
+       for s in suppliers:
+         a=s.find("a")
+         link=a['href']
+         name=a.getText()
+         entry=self.genEntry(name.strip(),link.strip())
+         self.SoupLiars.append(entry)
 
-  def getPage(self,letter,page):
-    #TODO
-    #Gets a page html text using the format self.BASE_URL+"/"+letter+"/"+page
-    #Returns None if past last page
-    return None
+    def getPage(self,letter,page):
+           #TODO
+           #Gets a page html text using the format self.BASE_URL+"/"+letter+"/"+page
+           #Returns None if past last page
+           return None
 
-  def harvest(self):
-    letters=[chr(l) for l in range(ord('A'),ord('Z')+1) ]
-    letters.append("1");
-    for letter in letters:
-      for page in range(1,101):
-        html=self.getPage(letter,page)
-        if html is None: 
-          continue
-        self.harvestSuppliersFromPage(html)
+    def harvest(self):
+      letters=[chr(l) for l in range(ord('A'),ord('Z')+1) ]
+      letters.append("1");
+      for letter in letters:
+        for page in range(1,101):
+          html=self.getPage(letter,page)
+          if html is None: 
+            continue
+          self.harvestSuppliersFromPage(html)
 
-  def dump(self):
-    for ent in self.SoupLiars:
-      print(ent)
+    def dump(self):
+      for ent in self.SoupLiars:
+        print(ent)
 
 
 
@@ -82,160 +83,6 @@ class HarvestIndustrialDirectory:
           continue
         self.harvestProductsFromPage(html)
 
-class HarvestSupplierProfile:
-  """
-  Harvest data for a single Supplier profile.
-  Stores result of harvest in 
-  ProFile = {
-    'about': ...
-    'catalog': ...
-    'news': ...
-    'announcements': ...
-    'articles': ...
-    'videos': ...
-  }
-  """
-  ProFile={}
-  def __init__(self):
-    pass
-  
-
-  def getHTML(self,supplier_name,category):
-    """
-    Gets the HTML page for the supplier and category, currently look in ./tmp/suppliers for data
-    @TODO get this from URL
-    """
-    ret=open("./tmp/suppliers/supplier."+category+"."+supplier_name+".html").read()
-    return ret
-  def getAbout(self,html):
-    soup=bs(html,'html.parser')
-    content=soup.find("div",attrs={'id':'main-content'})
-
-    title=content.find("div",attrs={'class':'page-title-container'}).find("h1").getText()
-    profile_text=content.find("div",attrs={'id':'sp-profile-text'})
-    about_video=content.find("div",attrs={'class':'sup-about-video'}).find('iframe')['src']
-    about={
-      'title':title,
-      'text':profile_text,
-      'video':about_video
-    }
-    return about
-  def getCatalog(self,html):
-    soup=bs(html,'html.parser')
-    content=soup.find("div",attrs={'id':'catalog-area-list'})
-    catalog=[]
-    for div in content.findAll('div',attrs={'class','catalog-area'}):
-      url=div.find('a')['href']
-      img=div.find('img')['src']
-      title=div.find('div',attrs={'class':'area-text'}).find('a').getText().strip()
-      subtext=div.find('div',attrs={'class':'subtext'}).getText().strip()
-      product={
-        'url':url,
-        'img':img,
-        'title':title,
-        'subtext':subtext
-      }
-      catalog.append(product);
-    return catalog
-  def getNews(self,html):
-    soup=bs(html,'html.parser')
-    content=soup.find("div",attrs={'class':'area-listing'})
-    news=[]
-    for item in content.findAll('div',attrs={'class','area-listing-item'}):
-      date=item.find('div',attrs={'class':'left'}).getText().strip()
-      title=item.find("a",attrs={'class':'item-title'}).getText().strip()
-      url=item.find("a",attrs={'class':'item-title'})['href']
-      desc=item.find("p").getText().strip()
-      news_item={
-        'title':title,
-        'date':date,
-        'link':url,
-        'desc':desc
-      }
-      news.append(news_item)
-    return news
-      
-  def getAnnouncements(self,html):
-    soup = bs(html,"html.parser")
-    #There is an extra </div> which breaks everything.
-    #Iterate 4 times instead and then join arrays (and hope they are in the same order)
-    titles=[]
-    descriptions=[]
-    images=[]
-    urls=[]
-    for title in soup.findAll("div",attrs={"class","item-title-container"}):
-        titles.append(title.getText().strip() )
-        link=title.find("a")['href']
-        urls.append(link)
-    for desc in soup.findAll("span",attrs={"class","short-desc"}):
-        descriptions.append(desc.getText().strip() )
-    for img in soup.findAll("div",attrs={"class":"pa-img-wrapper"}):
-        src=img.find("img")['src']
-        images.append(src)
-    #Now put in dictionary
-    announcements=[]
-    for i in range(0,len(titles)):
-        item={}
-        item['title']=titles[i]
-        item['description']=descriptions[i]
-        item['img']=images[i]
-        item['url']=urls[i]
-        announcements.append(item)
-    return(announcements)
-
-  def getArticles(self,html):
-      """
-      Gets a list of article headers from an html string
-      returns
-          Articles=[
-              {
-                  "title":""
-                  "description":""
-                  "subtext":""
-                  "url":""
-              },
-              {},...
-          ]
-      """
-      Articles=[]
-      soup=bs(html,'html.parser')
-      content=soup.find("div",attrs={'class':'area-listing'})
-      items=content.findAll("div",attrs={'class':'area-listing-item'})
-      for item in items:
-          title=item.find('a',attrs={'class':'item-title'}).getText().strip()
-          url=item.find('a',attrs={'class':'item-title'})['href']
-          subtext=item.find("b").getText().strip() 
-          desc=item.find("div",attrs={'class':'last'}).find("div").getText().strip()#TODO cleanup
-          Articles.append(
-              {
-                  'title':title,
-                  'url':url,
-                  'subtext':subtext,
-                  'description':desc
-              }
-          )
-      return Articles
-
-
-  def harvest(self,supplier_name):
-    categories=['about','catalog','news','productannouncements','techarticles','videos']
-    for category in categories:
-      html=self.getHTML(supplier_name,category)
-      if category =="about":
-        about=self.getAbout(html)
-        self.ProFile['about']=about
-      if category =="catalog":
-        catalog=self.getCatalog(html)
-        self.ProFile['catalog']=catalog
-      if category =="news":
-        news=self.getNews(html)
-        self.ProFile['news']=news
-      if category =="productannouncements":
-        announce=self.getAnnouncements(html)
-        self.ProFile['announcements']=announce
-
-  def dump(self):
-    print(self.ProFile)
 
 if __name__=='__main__':
   DoS=HarvestDirectoryOfSuppliers()
--- tests/test1.py
+++ tests/test1.py
@@ -7,12 +7,10 @@ def prt(txt):
 def get_html_string(fname):
     return open(fname).read()
 
-
 def test_announcements():
     html=get_html_string("./tmp/suppliers/supplier.productannouncements.ABBElectrificationProducts.html")
     SP = HarvestSupplierProfile()
     ans = SP.getAnnouncements(html)
-    
     #TODO make sure the order doesn't change, IE, that ans[N]['title'] corresponds to the correct ans[N]['desc,url,img]
     #Not sure, but there's a chance that bs gets the order wrong (prob fine though)
     #assert ans[0]['title']=="blah"
@@ -26,5 +24,52 @@ def test_articles():
     html=get_html_string("./tmp/suppliers/supplier.techarticles.ABBElectrificationProducts.html")
     SP = HarvestSupplierProfile()
     articles=SP.getArticles(html)
-    #assert articles[0]['title'] == "The Evolution of the Electronically Controlled Contactor"
-    #assert articles[0]['url'] == "/Goto/GotoWebPage?gotoUrl=http%3A%2F%2Fwww%2Eglobalspec%2Ecom%2FABBLowVoltageProductsSystems%2Fref%2FEvol%5FElctrnc%5FContctr%2Ehtm&gotoType=TechArticle&vid=98924&context=The%20Evolution%20of%20the%20Electronically%20Controlled%20Contactor&eb=false&fromSupplier=1"
+    assert articles[0]['title'] == "The Evolution of the Electronically Controlled Contactor"
+    assert articles[0]['url'] == "/Goto/GotoWebPage?gotoUrl=http%3A%2F%2Fwww%2Eglobalspec%2Ecom%2FABBLowVoltageProductsSystems%2Fref%2FEvol%5FElctrnc%5FContctr%2Ehtm&gotoType=TechArticle&vid=98924&context=The%20Evolution%20of%20the%20Electronically%20Controlled%20Contactor&eb=false&fromSupplier=1"
+
+
+def getVideos(html):
+    """
+    Gets a list of article headers from an html string
+    returns
+        Videos=[
+            {
+                "title":""
+                "description":""
+                "subtext":""
+                "url":""
+            },
+            {},...
+        ]
+    """
+    Videos=[]
+    soup=bs(html,'html.parser')
+    content=soup.find("div",attrs={'id':'featured-videos'})
+    #TODO (This is just the code for Article getting, needs the correct divs ids classes etc)
+    for i,vid in enumerate(content.findAll("div",attrs={'class':'videoPage'})):
+        title=vid.find("div",attrs={"class":"feature-title"}).getText().strip()
+        desc=vid.find("p")
+        subtext=vid.find("div",attrs={"class":"classification"})
+        #The above don't have a shared container, take from loop index i
+        url = content.findAll("iframe")[i]['src']
+        if subtext is not None:
+            subtext=subtext.getText().strip()
+        if desc is not None:
+            desc=desc.getText().strip()
+        Videos.append(
+            { 
+                'title':title,
+                'description':desc,
+                'subtext':subtext,
+                'url':url
+            }
+        )
+    return Videos
+
+
+def test_videos():
+    html=get_html_string("./tmp/suppliers/supplier.videos.ABBElectrificationProducts.html")
+    SP = HarvestSupplierProfile()
+    videos=getVideos(html)
+    assert videos[0]['title']=="ABB Miniature Circuit Breaker clearing downstream fault ."
+    assert videos[0]['url']=="https://www.youtube.com/embed/850aO98OAyI"
