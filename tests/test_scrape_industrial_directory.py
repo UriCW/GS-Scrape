7e0df2997f7e66a12f01ccd21b35c3ae069868c6
volcan <volcan@drek>
2016-12-12 03:50:39 +0000

    Merges some files into one long file (industrial directory index)

--- tests/test_scrape_industrial_directory.py
+++ tests/test_scrape_industrial_directory.py
@@ -6,7 +6,8 @@ from time import sleep
 from random import randint
 
 
-
+buffer_files="./tmp/queues/industrial_directory.{0}.json"
+base_url="http://www.globalspec.com/industrial-directory/browse/"
 
 def slp():
     """
@@ -21,13 +22,12 @@ def prt(txt):
 
 def buffer_letter(letter, start_page=1, last_page=999):
     buff=[]
-    buff_file="./tmp/queues/industrial_directory."+str(letter)+".json"
-    base_url="http://www.globalspec.com/industrial-directory/browse/"
+    buff_file=buffer_files.format(letter)
     
     for page in range(start_page,last_page):
         html=Helpers.get_directory_page(base_url,letter,page)
         #HTML contains one table, if we're past the last page, the table is empty
-        #This is a quick and dirty way to handle this
+        #This is a quick and (very) dirty way to handle this
         if "<tr>" not in html:
             prt("Past last page "+letter+"/"+str(page) )
             return buff
@@ -41,34 +41,28 @@ def buffer_letter(letter, start_page=1, last_page=999):
 
 
 def queue_up_industrial_directory():
-    base_url="http://www.globalspec.com/industrial-directory/browse/"
     letters=[chr(l) for l in range(ord('a'),ord('z')+1) ]
     letters.append("1")
-
-    queue = Helpers.FetchQue("./tmp/queues/industrial_directory.json")
-    
     for i,letter in enumerate(letters):
-        buff=buffer_letter(letter,1,999)
+        buff=buffer_letter(letter,1,3) #Only three pages for testing purposes
         slp()
-        """
-        for page in range(start_page,end_page+1):
-            print ("Letter: "+letter+" page "+str(page) )
-            html=Helpers.get_directory_page(base_url,letter,page)
-            #HTML contains one table, if we're past the last page, the table is empty
-            #This is a quick and dirty way to handle this
-            if "<tr>" not in html:
-                prt("Past last page "+letter+"/"+str(page) )
-                break
-            jon=DirectoryHarvesters.HarvestIndustrialDirectory().get(html)
-            #OK, we've got a list of entries, now queue them
-            prt(len(jon))
-            for ent in jon:
-                ent['harvested']=False
-                queue.add(ent)
-            #Sleepsome
-            slp()
-        """
 
+
+
+
+
+def test_merge_files():
+    letters=[chr(l) for l in range(ord('a'),ord('z')+1) ]
+    letters.append("1")
+    buff=[]
+    for let in letters:
+        fn=buffer_files.format(let)
+        with open(fn,"r") as bf:
+            txt=bf.read().replace("][",",")
+            data=json.loads(txt)
+            buff=buff+data
+    with open(buffer_files.format("index"),"w") as bf:
+        json.dump(buff,bf,sort_keys=True,indent=4)
 
 def d_test_letter_scrape():
     buffer_letter("z",1,3)
@@ -77,7 +71,7 @@ def d_test_letters_scrape():
     for let in ['a','b','c']:
         buffer_letter(let,1,5)
 
-def test_batch_scrape(): #Harvest all of the industrial directory
+def d_test_batch_scrape(): #Harvest all of the industrial directory
     queue_up_industrial_directory()
 
 def d_test_sleep():
